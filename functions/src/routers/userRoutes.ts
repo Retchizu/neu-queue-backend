8fb5b43c16559314fe5985cc0f09d4452c0ed140
Richmond Baltazar <baltazarrichmond31@gmail.com>
2026-01-24 23:32:03 +0800

    user authorization

--- functions/src/routers/userRoutes.ts
+++ functions/src/routers/userRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyAuthTokenAndDomain } from "../middlewares/verifyAuthTokenAndDomain";
 import { getCurrentAuthDetails } from "../controllers/userController";
+import { verifyRole } from "@/middlewares/verifyRole";
 
 // eslint-disable-next-line new-cap
 const router: Router = Router();
 
-router.get("/me", verifyAuthTokenAndDomain, getCurrentAuthDetails);
+router.get("/admin/me", verifyAuthTokenAndDomain, verifyRole(["admin"]), getCurrentAuthDetails);
+router.get("/cashier/me", verifyAuthTokenAndDomain, verifyRole(["cashier"]), getCurrentAuthDetails);
 
 
 export default router;
