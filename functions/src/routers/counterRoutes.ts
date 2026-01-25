f5b56db45f57efcf654fa85aecf47bf06343b820
Richmond Baltazar <baltazarrichmond31@gmail.com>
2026-01-25 21:30:46 +0800

    allow cashier to access get counter by station

--- functions/src/routers/counterRoutes.ts
+++ functions/src/routers/counterRoutes.ts
@@ -18,7 +18,7 @@ router.use(verifyAuthTokenAndDomain);
 
 router.post("/", verifyRole(["admin", "superAdmin"]), addCounter);
 router.get("/", verifyRole(["admin", "superAdmin"]), getCounters);
-router.get("/:stationId", verifyRole(["admin", "superAdmin"]), getCountersByStation);
+router.get("/:stationId", verifyRole(["admin", "superAdmin", "cashier"]), getCountersByStation);
 router.delete("/:counterId", verifyRole(["admin", "superAdmin"]), deleteCounter);
 router.put("/:counterId", verifyRole(["admin", "superAdmin"]), updateCounter);
 router.post("/:counterId/enter", verifyRole(["cashier"]), enterCounter);
